4d0db10545d87a5e606ff2e126e17b19da80f2ca
Nesta <nestaenow@gmail.com>
2022-01-31 17:58:20 +0100

    moving

--- Binding.js
+++ Binding.js
@@ -1,2 +1,16 @@
 let ten = 10;
-console.log(ten * ten);;
+console.log(ten * ten);
+
+//Allocating binding different values on runtime
+let lucaDebt = 500;
+lucaDebt = lucaDebt - 200;
+console.log(lucaDebt);
+
+//verifying the value of an uninitialised binding
+let butter;
+console.log(butter);
+
+//introducing var and const
+var name = 'Nesta';
+const greeting = 'Good Evening ';
+console.log(greeting + name);
---
+++ functions.js
@@ -0,0 +1,4 @@
+console.log(Math.max(2, 1, 6));
+console.log(Math.min(11, 31) + 100);
+
+//controlFlow
