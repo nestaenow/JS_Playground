931ca847046311ffadc4eee61c665ea64eb5f957
Nesta <nestaenow@gmail.com>
2022-01-31 14:43:08 +0100

    ifElse

--- Conditional.js
+++ Conditional.js
@@ -1,6 +1,15 @@
 let theNumber = Number(prompt("Pick a number"));
-if (!Number.isNaN(theNumber)) {
-    console.log("Your number is the square root of " + theNumber * theNumber);
+// if (!Number.isNaN(theNumber)) {
+//     console.log("Your number is the square root of " + theNumber * theNumber);
+// } else {
+//     console.log("Hey. Why didn't you give me a number?");
+// }
+
+//multiple if-else statements
+if (theNumber < 10){
+    console.log("Small");
+} else if (theNumber < 100){
+    console.log("Medium");
 } else {
-    console.log("Hey. Why didn't you give me a number?");
+    console.log("Large");
 }
