cbf642a42064b7c7d6ef4ee4c8710ed672daadbb
Nesta <nestaenow@gmail.com>
2022-02-02 10:54:49 +0100

    while loop

--- Loops.js
+++ Loops.js
@@ -6,3 +6,10 @@ console.log(6);
 console.log(8);
 console.log(10);
 console.log(12);
+
+//The above could be made easier by using a while loop
+let number = 0;
+while (number <= 12){
+    console.log(number);
+    number += 2;
+}
