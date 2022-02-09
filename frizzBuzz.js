de97e54a9fb84927355fa3f550708dbb307e1b78
Nesta <nestaenow@gmail.com>
2022-02-09 02:33:41 +0100

    FizzBuzz

--- frizzBuzz.js
+++ frizzBuzz.js
@@ -1,10 +1,14 @@
 for(let number = 1; number <= 100; number++){
-    if(number % 3 == 0){
-        console.log('Fizz');
+    if(number % 3 == 0 && number % 5 == 0){
+        console.log('FizzBuzz');
         continue;
     } 
     else if (number % 5 == 0){
         console.log('Buzz');
+        continue;
+    }
+    else if(number % 3 == 0){
+        console.log('Fizz');
         continue;
     }
     console.log(number);
