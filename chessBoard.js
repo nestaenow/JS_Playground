e2585cd59e9e90a07e482cba690e7c787391acc5
Nesta <nestaenow@gmail.com>
2022-02-09 12:08:19 +0100

    chessBoard

--- chessBoard.js
+++ chessBoard.js
@@ -1,13 +1,27 @@
-let hash = '';
-for(let i = 0; i < 4; i++){
-    hash = '';
-    for(let j = 0; j < 4; j++){
-        hash += ' #';
+// let hash = '';
+// let size = 8;
+// for(let i = 0; i < size/2; i++){
+//     hash = '';
+//     for(let j = 0; j < size/2; j++){
+//         hash += ' #';
+//     }
+//     console.log(hash);
+//     hash = '';
+//     for(let j = 0; j < size/2; j++){
+//         hash += '# ';
+//     }
+//     console.log(hash);
+// }
+let size = 8;
+let board = '';
+for(let i = 0; i < size; i++){
+    for(let j = 0; j < size; j++){
+        if((i + j) % 2 == 0){
+            board += ' ';
+        }else {
+            board += '#'
+        }
     }
-    console.log(hash);
-    hash = '';
-    for(let j = 0; j < 4; j++){
-        hash += '# ';
-    }
-    console.log(hash);
+    board += '\n';
 }
+console.log(board);
