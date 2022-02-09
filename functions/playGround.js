aac3ec5272287e21c070d4ac53147f217c2e3cda
Nesta <nestaenow@gmail.com>
2022-02-09 19:20:25 +0100

    functions play ground

--- functions/playGround.js
+++ functions/playGround.js
@@ -4,18 +4,27 @@
 
 // console.log(square(4));
 
-const makeNoise = function() {
-    console.log('Pling!');
+// const makeNoise = function() {
+//     console.log('Pling!');
+// }
+
+// makeNoise();
+
+// const power = function(base, exponent){
+//     let result = 1;
+//     for(let i = 0; i < exponent; i++){
+//         result *= base;
+//     }
+//     return result;
+// };
+
+// console.log(power(2, 10));
+
+let x = 10;
+if (true) {
+    let y = 20;
+    var z = 30;
+    console.log(x + y + z);
 }
 
-makeNoise();
-
-const power = function(base, exponent){
-    let result = 1;
-    for(let i = 0; i < exponent; count++){
-        result *= base;
-    }
-    return result;
-};
-
-console.log(power(2, 10));
+console.log(x + z);
