8f0e96e0a43e87aa058c58bfcc204d12efad42c3
Nesta <nestaenow@gmail.com>
2022-02-09 20:11:48 +0100

    arrowFunction

--- functions/playGround.js
+++ functions/playGround.js
@@ -20,11 +20,21 @@
 
 // console.log(power(2, 10));
 
-let x = 10;
-if (true) {
-    let y = 20;
-    var z = 30;
-    console.log(x + y + z);
-}
+// let x = 10;
+// if (true) {
+//     let y = 20;
+//     var z = 30;
+//     console.log(x + y + z);
+// }
 
-console.log(x + z);
+// console.log(x + z);
+
+//Arrow Function
+const power = (base, exponent) => {
+    let result = 1;
+    for (let count = 0; count < exponent; count++) {
+        result *= base;
+    }
+    return result;
+};
+console.log(power(2,10));
