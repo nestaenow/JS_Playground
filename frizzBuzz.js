fac2059fc96755a1afa347c58ff1acb2d55f7b3b
Nesta <nestaenow@gmail.com>
2022-02-09 02:27:27 +0100

    Frizz

--- frizzBuzz.js
+++ frizzBuzz.js
@@ -1,10 +1,10 @@
 for(let number = 1; number <= 100; number++){
-    // if(number % 3 == 0){
-    //     console.log('Fizz');
-    //     continue;
-    // } 
-    // else if (number % 5 == 0){
-        //     console.log('Buzz');
-        // }
+    if(number % 3 == 0){
+        console.log('Fizz');
+        continue;
+    } 
+    else if (number % 5 == 0){
+        console.log('Buzz');
+    }
     console.log(number);
 }
