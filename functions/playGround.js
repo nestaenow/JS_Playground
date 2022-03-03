c2ec8180bff376924ac5c292dc1ad6c13da82f44
Nesta <nestaenow@gmail.com>
2022-03-03 23:28:23 +0100

    min function

--- functions/playGround.js
+++ functions/playGround.js
@@ -137,17 +137,26 @@
 
 //An even better way to write this fucntions to make it more versatile
 //is as follows;
-function zeroPad(number, width) {
-    let string = String(number);
-    while (string.length < width) {
-        string = '0' + string;
-    }
-    return string;
-}
-function printFarmInventory(cows, chickens, pigs) {
-    console.log(`${zeroPad(cows, 3)} Cows`);
-    console.log(`${zeroPad(chickens, 3)} Chickens`);
-    console.log(`${zeroPad(pigs, 3)} Pigs`);
+// function zeroPad(number, width) {
+//     let string = String(number);
+//     while (string.length < width) {
+//         string = '0' + string;
+//     }
+//     return string;
+// }
+// function printFarmInventory(cows, chickens, pigs) {
+//     console.log(`${zeroPad(cows, 3)} Cows`);
+//     console.log(`${zeroPad(chickens, 3)} Chickens`);
+//     console.log(`${zeroPad(pigs, 3)} Pigs`);
+// }
+
+// printFarmInventory(7, 16, 3);
+
+const min = (a,b) => {
+    if(a > b)
+        return b;
+    else
+        return a;
 }
 
-printFarmInventory(7, 16, 3);
+console.log(min(10, 1)); 
