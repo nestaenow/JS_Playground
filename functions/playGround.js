29cc0896ae5aa0489ef3e2a4eac6467a6aa2903c
Nesta <nestaenow@gmail.com>
2022-03-03 20:20:50 +0100

    recursion

--- functions/playGround.js
+++ functions/playGround.js
@@ -86,4 +86,19 @@ function power(base, exponent) {
 }
 
 console.log(power(3, 3));
-//
+//recursion over loop better example
+function findSolution(target) {
+    function find(current, history) {
+        if (current == target) {
+            return history;
+        }else if (current > target){
+            return null;
+        }else {
+            return find(current + 5, `(${history} + 5)`) || 
+            find(current * 3, `(${history} * 3)`); 
+        }
+    }
+    return find(1, "1");
+}
+
+console.log(findSolution(13));
