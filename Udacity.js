a91ed34c68761bd42dc4fdaffdb8db75bb327c43
nestaenow <nestaenow@gmail.com>
2022-06-08 08:49:06 +0100

    liftoff

--- Udacity.js
+++ Udacity.js
@@ -28,26 +28,52 @@
 //     x = x + 1;
 // }
 
-let num = 99;
+// let num = 99;
 
-while (num >=1) {
-    // Last iteration. Note occurances of bottle, bottle, bottleS
-    if (num === 1) {
-        console.log(num + " bottle of juice on the wall! "
-                    + num + " bottle of juice! Take one down, pass it around... "
-                    + (num-1) + " bottles of juice on the wall!");
+// while (num >=1) {
+//     // Last iteration. Note occurances of bottle, bottle, bottleS
+//     if (num === 1) {
+//         console.log(num + " bottle of juice on the wall! "
+//                     + num + " bottle of juice! Take one down, pass it around... "
+//                     + (num-1) + " bottles of juice on the wall!");
+//     }
+//     // Second-last iteration. Note occurances of bottleS, bottleS, bottle
+//     else if (num === 2){
+//         console.log(num + " bottles of juice on the wall! "
+//                     + num + " bottles of juice! Take one down, pass it around... "
+//                     + (num-1) + " bottle of juice on the wall!");
+//     }
+//     // All other iterations. Note occurances of bottleS, bottleS, bottleS
+//     else {
+//         console.log(num + " bottles of juice on the wall! "
+//                     + num + " bottles of juice! Take one down, pass it around... "
+//                     + (num-1) + " bottles of juice on the wall!");
+//     }
+//     num = num - 1;
+// }
+
+let num = 60;
+
+while(num>=0){
+    if (num === 50) {
+        console.log('Orbiter transfers from ground to internal power');
     }
-    // Second-last iteration. Note occurances of bottleS, bottleS, bottle
-    else if (num === 2){
-        console.log(num + " bottles of juice on the wall! "
-                    + num + " bottles of juice! Take one down, pass it around... "
-                    + (num-1) + " bottle of juice on the wall!");
+    else if (num === 31) {
+        console.log("Ground launch sequencer is go for auto sequence start");
     }
-    // All other iterations. Note occurances of bottleS, bottleS, bottleS
-    else {
-        console.log(num + " bottles of juice on the wall! "
-                    + num + " bottles of juice! Take one down, pass it around... "
-                    + (num-1) + " bottles of juice on the wall!");
+    else if (num === 16) {
+        console.log("Activate launch pad sound suppression system");
     }
+    else if (num === 10) {
+        console.log("Activate main engine hydrogen burnoff system");
+    }
+    else if (num === 6) {
+        console.log("Main engine start");
+    }else if (num === 0) {
+        console.log('Solid rocket booster ignition and liftoff!');
+    }else{
+        console.log('T-' + num +' seconds');    
+    }
+
     num = num - 1;
 }
