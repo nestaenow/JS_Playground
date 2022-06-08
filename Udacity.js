50f277b8995694a53c737a4a3e2e496e646f342e
nestaenow <nestaenow@gmail.com>
2022-06-08 08:26:34 +0100

    juice

--- Udacity.js
+++ Udacity.js
@@ -11,18 +11,43 @@
 
 // Let us print
 // console.log(ASCII_value);
-let x = 1;
-const three = 3;
-const five = 5;
-while(x <= 20){
-    if(x % three === 0 && x % five === 0){
-        console.log('JuliaJames');
-    }else if(x % three === 0){
-        console.log('Julia');
-    }else if(x % five === 0){
-        console.log('James');
-    }else{
-        console.log(x);
+
+// let x = 1;
+// const three = 3;
+// const five = 5;
+// while(x <= 20){
+//     if(x % three === 0 && x % five === 0){
+//         console.log('JuliaJames');
+//     }else if(x % three === 0){
+//         console.log('Julia');
+//     }else if(x % five === 0){
+//         console.log('James');
+//     }else{
+//         console.log(x);
+//     }
+//     x = x + 1;
+// }
+
+let num = 99;
+
+while (num >=1) {
+    // Last iteration. Note occurances of bottle, bottle, bottleS
+    if (num === 1) {
+        console.log(num + " bottle of juice on the wall! "
+                    + num + " bottle of juice! Take one down, pass it around... "
+                    + (num-1) + " bottles of juice on the wall!");
     }
-    x = x + 1;
+    // Second-last iteration. Note occurances of bottleS, bottleS, bottle
+    else if (num === 2){
+        console.log(num + " bottles of juice on the wall! "
+                    + num + " bottles of juice! Take one down, pass it around... "
+                    + (num-1) + " bottle of juice on the wall!");
+    }
+    // All other iterations. Note occurances of bottleS, bottleS, bottleS
+    else {
+        console.log(num + " bottles of juice on the wall! "
+                    + num + " bottles of juice! Take one down, pass it around... "
+                    + (num-1) + " bottles of juice on the wall!");
+    }
+    num = num - 1;
 }
