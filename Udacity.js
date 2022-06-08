90b0bd5547df0ce509778d6a9c4316f725efe53c
nestaenow <nestaenow@gmail.com>
2022-06-08 08:05:36 +0100

    while loop

--- Udacity.js
+++ Udacity.js
@@ -4,10 +4,25 @@
 // console.log(dinner);
 
 // Pick a string. Your string can have any number of characters.
-const my_string = "a";
+// const my_string = "a";
 
 // Calculate the ASCII value of the first character, i.e. the character at the position 0. 
-const ASCII_value = my_string.charCodeAt(0);
+// const ASCII_value = my_string.charCodeAt(0);
 
 // Let us print
-console.log(ASCII_value);
+// console.log(ASCII_value);
+let x = 1;
+const three = 3;
+const five = 5;
+while(x <= 20){
+    if(x % three === 0 && x % five === 0){
+        console.log('JuliaJames');
+    }else if(x % three === 0){
+        console.log('Julia');
+    }else if(x % five === 0){
+        console.log('James');
+    }else{
+        console.log(x);
+    }
+    x = x + 1;
+}
