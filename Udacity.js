3f16003edf159c980e99d9327e09612b67e75fc6
nestaenow <nestaenow@gmail.com>
2022-06-21 18:53:32 +0100

    progress

--- Udacity.js
+++ Udacity.js
@@ -1,4 +1,13 @@
 // Variables
-let lunch = "tacos";
-const dinner =  "tamales";
-console.log(dinner);
+// let lunch = "tacos";
+// const dinner =  "tamales";
+// console.log(dinner);
+
+// Pick a string. Your string can have any number of characters.
+const my_string = "a";
+
+// Calculate the ASCII value of the first character, i.e. the character at the position 0. 
+const ASCII_value = my_string.charCodeAt(0);
+
+// Let us print
+console.log(ASCII_value);
