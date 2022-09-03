548d4743d578c3d0fb385ea06c298b1c67e9cccd
nestaenow <nestaenow@gmail.com>
2022-09-03 23:28:53 +0100

    Added a function "randomWholeNum"

--- random.js
+++ random.js
@@ -1,5 +1,10 @@
 function randomFraction() {
-  return Math.random();
+    //returns random decimal number between 0 and 1
+    return Math.random();
 }
  
+function randomWholeNum() {
+    //returns random whole numbers between 0 and 9
+    return Math.floor(Math.random() * 10);
+}
 randomFraction()
