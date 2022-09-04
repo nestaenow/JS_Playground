f0f9b79038bbbd149559f02e96b42e5f11c3033b
nestaenow <nestaenow@gmail.com>
2022-09-04 20:55:06 +0100

    Added a comment

--- Conditional.js
+++ Conditional.js
@@ -26,6 +26,7 @@ console.log(checkEqual(1, 2));
 
 //This function used the multiple tenary operators to check the sign of the number inputed
 function checkSign(num) {
+    //the brackets around conditional statement is just to make code more readable
     return (num > 0) ? "positive" 
         : (num < 0) ? "negative" 
         : "zero";
