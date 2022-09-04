6e5823f2b307220bf06d865c5dc759254b723f32
nestaenow <nestaenow@gmail.com>
2022-09-04 20:17:10 +0100

    Added a function "checkSign"

--- Conditional.js
+++ Conditional.js
@@ -23,3 +23,11 @@ function checkEqual(a, b) {
 }
 
 checkEqual(1, 2);
+
+function checkSign(num) {
+  return (num > 0) ? "positive" 
+    : (num < 0) ? "negative" 
+    : "zero";
+}
+
+checkSign(10);
