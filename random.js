a7b7f36c9ee934a668f95271cbabba5f36245df0
nestaenow <nestaenow@gmail.com>
2022-09-04 19:50:16 +0100

    Added a function "randomRange"

--- random.js
+++ random.js
@@ -7,4 +7,9 @@ function randomWholeNum() {
     //returns random whole numbers between 0 and 9
     return Math.floor(Math.random() * 10);
 }
+
+function randomRange(myMin, myMax) {
+    //returns a random whole number between myMin and myMax
+    return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+}
 randomFraction()
