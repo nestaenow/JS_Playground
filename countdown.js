f9d0f30cfece5e91f815bb3568c9cb901c4cb146
nestaenow <nestaenow@gmail.com>
2022-09-06 20:34:53 +0100

    Added a comment describing what the function returns

--- countdown.js
+++ countdown.js
@@ -2,9 +2,11 @@ function countdown(n){
   if (n < 1) {
     return [];
   }else{
-    let countArray;
-    countArray.push(n);
-    countdown(n-1);
-    return countArray;
+    //returns an array with a countdown from 'n'
+    const arr = countdown(n - 1);
+    arr.unshift(n);
+    return arr;
   }
 }
+
+console.log(countdown(10));
