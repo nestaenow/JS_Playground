46a373f56721ceb77900cc4f789a58bba6d226d4
nestaenow <nestaenow@gmail.com>
2022-09-07 22:19:43 +0100

    Deleted comments

--- Conditional.js
+++ Conditional.js
@@ -1,23 +1,3 @@
-// let theNumber = Number(prompt("Pick a number"));
-// if (!Number.isNaN(theNumber)) {
-//     console.log("Your number is the square root of " + theNumber * theNumber);
-// } else {
-//     console.log("Hey. Why didn't you give me a number?");
-// }
-
-//multiple if-else statements
-// if (theNumber < 1){
-//     console.log("X-small");
-// } else if (theNumber < 10){
-//     console.log("Small");
-// } else if (theNumber < 100){
-//     console.log("Medium");
-// } else if(theNumber < 1000){
-//     console.log("Large");
-// } else {
-//     console.log("X-large");
-// }
-
 function checkEqual(a, b) {
   return a === b ? "Equal" : "Not Equal";
 }
